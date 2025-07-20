0609cb0a3050a51fdf57b54e298c2a3baa3c1e13
abhi2811mishra <24UCC203@lnmiit.ac.in>
2025-07-20 23:08:51 +0530

    done

--- lib/wifi_model.dart
+++ lib/wifi_model.dart
@@ -1,4 +1,4 @@
-import 'dart:convert'; // Required for base64 decoding
+import 'dart:convert';
 
 class WiFiNetwork {
   final String ssid;
@@ -13,7 +13,7 @@ class WiFiNetwork {
     required this.longitude,
   });
 
-  // Factory constructor to create a WiFiNetwork object from a JSON map
+  
   factory WiFiNetwork.fromJson(Map<String, dynamic> json) {
     return WiFiNetwork(
       ssid: json['ssid'] as String,
@@ -23,12 +23,12 @@ class WiFiNetwork {
     );
   }
 
-  // Method to decode the base64 encrypted password
+  
   String get decryptedPassword {
     try {
       return utf8.decode(base64.decode(passwordEncrypted));
     } catch (e) {
-      // Handle decoding errors gracefully
+      
       print('Error decoding password for SSID $ssid: $e');
       return 'Decoding Error';
     }
